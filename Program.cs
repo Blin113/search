f461305112d146bf94b3a2f30e251a8bcb5ce87f
Blin113 <54438172+Blin113@users.noreply.github.com>
2020-01-27 22:13:36 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,39 +11,30 @@ namespace search
         {
             List<Person> personlista = new List<Person>();
 
-            personlista.Add(new Person("Armin Rezaie", 17, "Tjädervägen 11", 20020822));
-            personlista.Add(new Person("Test person", 17, "testväg 1", 20020101));
+            personlista.Add(new Person("Test Person0", 17, "testväg 0", 20020101));
+            personlista.Add(new Person("Test Person1", 17, "testväg 1", 20020202));
+            personlista.Add(new Person("Test Person2", 17, "testväg 2", 20020303));
+            personlista.Add(new Person("Test Person3", 17, "testväg 3", 20020404));
+            personlista.Add(new Person("Test Person4", 17, "testväg 4", 20020505));
+            personlista.Add(new Person("Test Person5", 17, "testväg 5", 20020606));
+            personlista.Add(new Person("Test Person6", 17, "testväg 6", 20020707));
 
-            Console.WriteLine("Search: "+accumulator+"\n");
-            
-            var info = Console.ReadKey();
-            Console.Clear();
-            switch (info.Key)
-            {
-                case ConsoleKey.Backspace:
-                    if (accumulator.Length == 0){
-                        break;
-                    }
-                    accumulator = accumulator.Remove(accumulator.Length - 1); 
-                    break;
-                
-                case ConsoleKey.Space:
-                    accumulator += " ";
+            Console.SetCursorPosition(0, 0);
+            Searchbar();
 
-                default:
-                    accumulator += info.Key.ToString().ToLower();
-                    break;
-            }
+
+
+
 
             List<int> result = LinSearch(personlista, accumulator);
 
-            foreach(int index in result)
+            foreach (int index in result)
             {
+                Console.WriteLine("\n_____________________________________________________________________________");
                 Console.WriteLine("Name: " + personlista[index].Name);
                 Console.WriteLine("Age: " + personlista[index].Age);
                 Console.WriteLine("Adress: " + personlista[index].Adress);
                 Console.WriteLine("Social security number: " + personlista[index].Personnum + "****");
-                Console.WriteLine();
             }
             Main(null);
         }
@@ -52,14 +43,14 @@ namespace search
         {
             Key = Key.ToLower();
             List<int> result = new List<int>();
-            for (int i = 0; i < pl.Count; i++) 
+            for (int i = 0; i < pl.Count; i++)
             {
-                if (Key == pl[i].Personnum+"")
+                if (pl[i].Personnum.ToString().Contains(Key + ""))
                 {
                     result.Add(i);
                 }
-                
-                else if (Key == pl[i].Age+"")
+
+                else if (pl[i].Age.ToString().Contains(Key + ""))
                 {
                     result.Add(i);
                 }
@@ -75,6 +66,167 @@ namespace search
                 }
             }
             return result;
+        }
+
+        public static void Searchbar()
+        {
+            Console.Write("Search: ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write(accumulator);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            var info = Console.ReadKey();
+            Console.Clear();
+            switch (info.Key)
+            {
+                case ConsoleKey.Backspace:
+                    if (accumulator.Length == 0)
+                    {
+                        break;
+                    }
+                    accumulator = accumulator.Remove(accumulator.Length - 1);
+                    break;
+
+                case ConsoleKey.Spacebar:
+                    accumulator += " ";
+                    break;
+
+                case ConsoleKey.D0:
+                    accumulator += "0";
+                    break;
+
+                case ConsoleKey.D1:
+                    accumulator += "1";
+                    break;
+
+                case ConsoleKey.D2:
+                    accumulator += "2";
+                    break;
+
+                case ConsoleKey.D3:
+                    accumulator += "3";
+                    break;
+
+                case ConsoleKey.D4:
+                    accumulator += "4";
+                    break;
+
+                case ConsoleKey.D5:
+                    accumulator += "5";
+                    break;
+
+                case ConsoleKey.D6:
+                    accumulator += "6";
+                    break;
+
+                case ConsoleKey.D7:
+                    accumulator += "7";
+                    break;
+
+                case ConsoleKey.D8:
+                    accumulator += "8";
+                    break;
+
+                case ConsoleKey.D9:
+                    accumulator += "9";
+                    break;
+
+                case ConsoleKey.NumPad0:
+                    accumulator += "0";
+                    break;
+
+                case ConsoleKey.NumPad1:
+                    accumulator += "1";
+                    break;
+
+                case ConsoleKey.NumPad2:
+                    accumulator += "2";
+                    break;
+
+                case ConsoleKey.NumPad3:
+                    accumulator += "3";
+                    break;
+
+                case ConsoleKey.NumPad4:
+                    accumulator += "4";
+                    break;
+
+                case ConsoleKey.NumPad5:
+                    accumulator += "5";
+                    break;
+
+                case ConsoleKey.NumPad6:
+                    accumulator += "6";
+                    break;
+
+                case ConsoleKey.NumPad7:
+                    accumulator += "7";
+                    break;
+
+                case ConsoleKey.NumPad8:
+                    accumulator += "8";
+                    break;
+
+                case ConsoleKey.NumPad9:
+                    accumulator += "9";
+                    break;
+
+                case ConsoleKey.Oem7:
+                    accumulator += "ä";
+                    break;
+
+                case ConsoleKey.Oem3:
+                    accumulator += "ö";
+                    break;
+
+                case ConsoleKey.Oem6:
+                    accumulator += "å";
+                    break;
+
+                case ConsoleKey.Oem4:
+                    accumulator += "´";
+                    break;
+
+                case ConsoleKey.Oem1:
+                    accumulator += "¨";
+                    break;
+
+                case ConsoleKey.Oem2:
+                    accumulator += "'";
+                    break;
+
+                case ConsoleKey.Divide:
+                    accumulator += "/";
+                    break;
+
+                case ConsoleKey.Subtract:
+                    accumulator += "-";
+                    break;
+
+                case ConsoleKey.Multiply:
+                    accumulator += "*";
+                    break;
+
+                case ConsoleKey.OemPlus:
+                    accumulator += "+";
+                    break;
+
+                case ConsoleKey.Add:
+                    accumulator += "+";
+                    break;
+
+                case ConsoleKey.Enter:
+                    accumulator += "";
+                    break;
+
+                default:
+                    Console.Clear();
+                    accumulator += info.Key.ToString().ToLower();
+                    break;
+            }
+
         }
     }
 }
